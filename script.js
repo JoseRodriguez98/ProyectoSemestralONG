645c3fa43ce965a64ad67091c40e1647161a381a
JoseRodriguez98 <joj.rodriguez@duocuc.cl>
2024-05-22 23:37:22 -0400

    ENTREGA NUMERO 2 LISTA
    FORMULARIO LISTO
    2 FUNCIONES (MODO OSCURO Y AUMENTO PEQUEÑO DEL TAMAÑO DE LAS IMAGENES Y CASI TODOS LOS BOTONES)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//PRIMERA FUNCION SOLICITADA
 //aquí está la primera funcion pedida que activa el formato oscuro
 function modoNocturno() {
     console.log("entro aqui");
@@ -13,4 +13,17 @@ function modoNocturno() {
         modal.classList.toggle('modo-noc');
     })
 }
+
+//SEGUNDA FUNCION SOLICITADA
+//con esta funcion al pasar por encima de las imagenes y los botones se van a agrandar un poco
+$(document).ready(function() {
+    $('.zoom').hover(
+        function() {
+            $(this).css('transform', 'scale(1.1)');
+        }, 
+        function() {
+            $(this).css('transform', 'scale(1)');
+        }
+    );
+});
 
